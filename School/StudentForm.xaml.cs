23029a188b371b2491058d82ce716cbd8b555b91
David Barth <davidbarth80@gmail.com>
2016-11-09 13:32:05 +0000

    sceleton project

---
+++ School/StudentForm.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+namespace School
+{
+    /// <summary>
+    /// Interaction logic for StudentForm.xaml
+    /// </summary>
+    public partial class StudentForm : Window
+    {
+        #region Predefined code
+
+        public StudentForm()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        #endregion
+    }
+}
