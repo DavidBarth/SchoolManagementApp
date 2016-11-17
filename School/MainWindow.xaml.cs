735460b7965011aac2548fc8107075f216fd4a0d
David Barth <davidbarth80@gmail.com>
2016-11-17 04:51:34 +0000

    Refractor and validate

--- School/MainWindow.xaml.cs
+++ School/MainWindow.xaml.cs
@@ -6,6 +6,8 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using School.Data;
+using System.Data;
+using System.Data.Objects;
 
 
 namespace School
@@ -62,54 +64,47 @@ namespace School
                 // If the user pressed Enter, edit the details for the currently selected student
                 case Key.Enter: Student student = this.studentsList.SelectedItem as Student;
 
-                    // Use the StudentsForm to display and edit the details of the student
-                    StudentForm sf = new StudentForm();
-
-                    // Set the title of the form and populate the fields on the form with the details of the student           
-                    sf.Title = "Edit Student Details";
-                    sf.firstName.Text = student.FirstName;
-                    sf.lastName.Text = student.LastName;
-                    sf.dateOfBirth.Text = student.DateOfBirth.ToString("d"); // Format the date to omit the time element
-
-                    // Display the form
-                    if (sf.ShowDialog().Value)
-                    {
-                        // When the user closes the form, copy the details back to the student
-                        student.FirstName = sf.firstName.Text;
-                        student.LastName = sf.lastName.Text;
-                        student.DateOfBirth = DateTime.Parse(sf.dateOfBirth.Text);
-
-                        // Enable saving (changes are not made permanent until they are written back to the database)
-                        saveChanges.IsEnabled = true;
-                    }
+                    editStudent(student);
+                    
                     break;
 
 
                 // if the the user pressed Insert create new StudentForm 
-                case Key.Insert : sf = new StudentForm();
-                    sf.Title = "New student for Class" + teacher.Class;
-                    
-                    //display the from
-                    if (sf.ShowDialog().Value)
-                    {
-                        //creates new object
-                        Student newStudent = new Student();
-                        newStudent.FirstName = sf.firstName.Text;
-                        newStudent.LastName = sf.lastName.Text;
-                        newStudent.DateOfBirth = DateTime.Parse(sf.dateOfBirth.Text);
-                        newStudent.Teacher = this.teacher; //assign student to current teacher
-                        saveChanges.IsEnabled = true;
-                    }
+                case Key.Insert: insertStudent();
                     break;
 
                 case Key.Delete: student = this.studentsList.SelectedItem as Student;
-                    MessageBoxResult mbr = MessageBox.Show(string.Format("Remove {0}", student.FirstName + " " + student.LastName +"?"),
-                        "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
-                    saveChanges.IsEnabled = true;
-                    schoolContext.Students.DeleteObject(student);
+                    removeStudent(student);
                     break;
 
 
+            }
+        }
+
+        private void removeStudent(Student student)
+        {
+            MessageBoxResult mbr = MessageBox.Show(string.Format("Remove {0}", student.FirstName + " " + student.LastName + "?"),
+                       "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+            saveChanges.IsEnabled = true;
+            schoolContext.Students.DeleteObject(student);
+        }
+
+        private void insertStudent()
+        {
+           StudentForm sf = new StudentForm();
+            sf.Title = "New student for Class" + teacher.Class;
+
+            //display the from
+            if (sf.ShowDialog().Value)
+            {
+                //creates new object
+                Student newStudent = new Student();
+                newStudent.FirstName = sf.firstName.Text;
+                newStudent.LastName = sf.lastName.Text;
+                newStudent.DateOfBirth = DateTime.Parse(sf.dateOfBirth.Text);
+                this.teacher.Students.Add(newStudent);
+                this.studentsInfo.Add(newStudent);
+                saveChanges.IsEnabled = true;
             }
         }
 
@@ -117,12 +112,62 @@ namespace School
 
         private void studentsList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
+            editStudent(this.studentsList.SelectedItem as Student);
+        }
 
+        private void editStudent(Student student)
+        {
+            // Use the StudentsForm to display and edit the details of the student
+            StudentForm sf = new StudentForm();
+
+            // Set the title of the form and populate the fields on the form with the details of the student           
+            sf.Title = "Edit Student Details";
+            sf.firstName.Text = student.FirstName;
+            sf.lastName.Text = student.LastName;
+            sf.dateOfBirth.Text = student.DateOfBirth.ToString("d"); // Format the date to omit the time element
+
+            // Display the form
+            if (sf.ShowDialog().Value)
+            {
+                // When the user closes the form, copy the details back to the student
+                student.FirstName = sf.firstName.Text;
+                student.LastName = sf.lastName.Text;
+                student.DateOfBirth = DateTime.Parse(sf.dateOfBirth.Text);
+
+                // Enable saving (changes are not made permanent until they are written back to the database)
+                saveChanges.IsEnabled = true;
+            }
         }
 
         // Save changes back to the database and make them permanent
         private void saveChanges_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                this.schoolContext.SaveChanges();
+                saveChanges.IsEnabled = false;
+            }
+
+             //if the user has changed the same student earlier , the overwrite thei changes with the new data
+            catch (OptimisticConcurrencyException)
+            {
+                this.schoolContext.Refresh(RefreshMode.StoreWins, schoolContext.Students);
+                this.schoolContext.SaveChanges();
+            }
+
+              // if some sort of db exception has occured, then display the reason for rollback
+            catch (UpdateException uEX)
+            {
+                MessageBox.Show(uEX.InnerException.Message, "Error saving changes");
+                this.schoolContext.Refresh(RefreshMode.StoreWins, schoolContext.Students);
+            }
+
+               
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error saving changes");
+                this.schoolContext.Refresh(RefreshMode.ClientWins, schoolContext.Students);
+            }
 
         }
 
